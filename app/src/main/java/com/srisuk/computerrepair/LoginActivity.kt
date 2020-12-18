b54ca2062eacd51d06f4db0d40eea77c80ea1954
srisuk013 <59003413+srisuk013@users.noreply.github.com>
2020-12-18 23:13:34 +0700

    update hideSoftKeyboard to LoginActivity

--- app/src/main/java/com/srisuk/computerrepair/LoginActivity.kt
+++ app/src/main/java/com/srisuk/computerrepair/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.srisuk.computerrepair
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.srisuk.computerrepair.ui.hideSoftKeyboard
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        root_Layout.setOnClickListener { hideSoftKeyboard() }
     }
 }
