72c7860ad70ead0a62ed5de9e5b2d258ae47a835
SAFE <safesai@hotmail.con>
2020-12-19 16:31:02 +0700

    history_view v3

--- app/src/main/java/com/srisuk/computerrepair/HistoryFragment.kt
+++ app/src/main/java/com/srisuk/computerrepair/HistoryFragment.kt
@@ -3,7 +3,7 @@ package com.srisuk.computerrepair
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class HistoryActivity : AppCompatActivity() {
+class HistoryFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_history)
