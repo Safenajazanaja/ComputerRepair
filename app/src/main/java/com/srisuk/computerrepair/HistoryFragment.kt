ceb77acec527393dd3cb597fd692e4fc0a50c47b
srisuk013 <59003413+srisuk013@users.noreply.github.com>
2020-12-19 17:07:24 +0700

    update  History

--- app/src/main/java/com/srisuk/computerrepair/HistoryFragment.kt
+++ app/src/main/java/com/srisuk/computerrepair/HistoryFragment.kt
@@ -1,17 +1,7 @@
 package com.srisuk.computerrepair
 
-<<<<<<< HEAD
 import androidx.fragment.app.Fragment
 
 class HistoryFragment: Fragment(R.layout.fragment_history) {
-=======
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class HistoryFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_history)
-    }
->>>>>>> safe
 }
--- app/src/main/java/com/srisuk/computerrepair/RepairFragment.kt
+++ app/src/main/java/com/srisuk/computerrepair/RepairFragment.kt
@@ -2,5 +2,5 @@ package com.srisuk.computerrepair
 
 import androidx.fragment.app.Fragment
 
-class RepairFragment : Fragment(R.layout.fragment_profile){
+class RepairFragment : Fragment(R.layout.fragment_repair){
 }
