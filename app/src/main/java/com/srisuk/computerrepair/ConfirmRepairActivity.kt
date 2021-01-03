ca56ea6f21f4669eebcf180243168f6bfe488cc6
srisuk013 <59003413+srisuk013@users.noreply.github.com>
2021-01-03 21:37:42 +0700

    update ConfirmRepairActivity

---
+++ app/src/main/java/com/srisuk/computerrepair/ConfirmRepairActivity.kt
@@ -0,0 +1,13 @@
+package com.srisuk.computerrepair
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmRepairActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_repair)
+
+
+    }
+}
